48b4a9352df93ee0ec436ed8fb3d303f585081f4
Acalin10 <calinmares10@gmail.com>
2019-11-19 16:30:45 +0100

    - Detecting the placement of a single ship on a grid

--- Detect shape/shipPlacement.py
+++ Detect shape/shipPlacement.py
@@ -1,6 +1,35 @@
 import pygame; import cv2; import numpy as np;
-from blob import extract_blobs;
 
+def extract_blobs(binary_image):
+    blobs = []
+    for y in range(0, binary_image.shape[0]):
+        for x in range(0, binary_image.shape[1]):
+            if binary_image[y, x] > 0:
+                binary_image[y, x] = 0
+                blob_pixels = []
+                queue = [[y, x]]
+
+                while len(queue) > 0:
+
+                    y_temp = queue[0][0]
+                    x_temp = queue[0][1]
+
+                    if x_temp + 1 < binary_image.shape[1] and binary_image[y_temp, x_temp + 1] > 0:
+                        binary_image[y_temp, x_temp + 1] = 0
+                        queue.append([y_temp, x_temp + 1])
+                    if y_temp + 1 < binary_image.shape[0] and binary_image[y_temp + 1, x_temp] > 0:
+                        binary_image[y_temp + 1, x_temp] = 0
+                        queue.append([y_temp + 1, x_temp])
+                    if x_temp - 1 > 0 and binary_image[y_temp, x_temp - 1] > 0:
+                        binary_image[y_temp, x_temp - 1] = 0
+                        queue.append([y_temp, x_temp - 1])
+                    if y_temp - 1 > 0 and binary_image[y_temp - 1, x_temp] > 0:
+                        binary_image[y_temp - 1, x_temp] = 0
+                        queue.append([y_temp - 1, x_temp])
+
+                    blob_pixels.append(queue.pop(0))
+                blobs.append(blob_pixels)
+    return blobs
 def threshold_red(image):
     result = image.copy()
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
