2ec3918b09a4ac1af38665d37e9ee887f12a5dac
Mikkel Sang Mee Baunsgaard <mikkelsangmeebaunsgaard@h350.aau1x.klient2.satellit.site.aau.dk>
2019-12-05 15:48:33 +0100

    Find white blobs and crop image

--- Final code/calibration.py
+++ Final code/calibration.py
@@ -3,41 +3,57 @@ import cv2
 import numpy as np
 from blob import extract_blobs
 
-image = cv2.imread("/Users/mikkelsangmeebaunsgaard/Desktop/blobs.jpg")
+image = cv2.imread("/Users/mikkelsangmeebaunsgaard/Desktop/mikkelTHIS.jpg")
+
+def coordinates(blobs):
+    coordinates = []
+    for blob in blobs:
+        x = []
+        y = []
+        for pixel in blob:
+            x.append(pixel[-1])
+            y.append(pixel[0])
+        x.sort(); y.sort()
+
+        coordinates.append(((x[0], y[0]), (x[-1], y[-1])))
+    return coordinates
 
 def calibration(image_to_crop):
     # Blur image
-    blurred_image = cv2.cvtColor(image_to_crop, cv2.COLOR_BGR2RGB)
-    blurred_image = cv2.GaussianBlur(image_to_crop, (5, 5), cv2.BORDER_DEFAULT)
+    blurred_image = cv2.cvtColor(image_to_crop, cv2.COLOR_BGR2GRAY)
+    blurred_image = cv2.GaussianBlur(blurred_image, (21, 21), cv2.BORDER_DEFAULT)
 
     # Threshold and mask white
-    lower_white = np.array([100, 120, 50]); upper_white = np.array([255, 255, 255])
-    binary_image = cv2.inRange(blurred_image, lower_white, upper_white)
+    #lower_white = np.array([90, 90, 90]); upper_white = np.array([255, 255, 255])
 
+    binary_image = cv2.inRange(blurred_image, 115, 255)
+    cv2.imshow("gray", blurred_image)
     cv2.imshow("mask", binary_image)
     # Find blob
     image_blobs = extract_blobs(binary_image)
     #image_blobs.pop()
 
-    """
+    correct_blobs = []
+
     for blob in image_blobs:
-        tempx = blob[-1][1] - blob[0][1]
-        tempy = blob[-1][0] - blob[0][0]
-        print(tempx, tempy)
-        if (abs(tempx/tempy) < 0.85):
-            image_blobs.remove(blob)
-    """
-    image_blobs.pop()
+        if len(blob) > 50000:
+            correct_blobs.append(blob)
 
+    for blob in correct_blobs:
+        print(blob[0], blob[-1])
 
+    coord = coordinates(correct_blobs)
+
+    #for blob in image_blobs:
+    #    print(blob[0], blob[-1])
 
     # Find placement grids
-    player1_placement = (image_blobs[0][0], image_blobs[0][-1])
-    player2_placement = (image_blobs[-1][0], image_blobs[-1][-1])
+    player1_placement = (coord[0][0], coord[0][-1])
+    player2_placement = (coord[-1][0], coord[-1][-1])
 
     # Find attack grids
-    player1_attack = (image_blobs[1][0], image_blobs[1][-1])
-    player2_attack = (image_blobs[2][0], image_blobs[2][-1])
+    player1_attack = (coord[1][0], coord[1][-1])
+    player2_attack = (coord[2][0], coord[2][-1])
 
     # Return the coordinates to the function, hence turning the function into these values
     # These values can be accessed by refering to them as an array
@@ -47,6 +63,7 @@ def calibration(image_to_crop):
 video = cv2.VideoCapture(1)
 video.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
 video.set(cv2.CAP_PROP_EXPOSURE, -3)
+
 temp = 0
 while True:
     check, frame = video.read()
@@ -56,7 +73,8 @@ while True:
 
     temp += 1
 
-cv2.imshow("frame", frame)
+#cv2.imshow("frame", frame)
+#cv2.imwrite("/Users/mikkelsangmeebaunsgaard/Desktop/redblobs.jpg", frame)
 positions = calibration(frame)
 print(positions)
 video.release()
--- Final code/cropAndDetect.py
+++ Final code/cropAndDetect.py
@@ -14,47 +14,44 @@ def detectShapes(image, player : int, type : int, cord):
 
     if player == 1:
         if type == 0:
-            cropped_image = image[cord[1][0][0][0]:cord[1][0][1][0], cord[1][0][0][1]:cord[1][0][1][1]]
+            cropped_image = image[cord[0][0][0][1]:cord[0][0][1][1], cord[0][0][0][0]:cord[0][0][1][0]]
             #binary_image = cv2.inRange(cropped_image, lower_red, higher_red)
-            ship_positions = extract_blobs(binary_image)
-            cv2.imshow("Cropped", binary_image)
+            #ship_positions = extract_blobs(binary_image)
+            cv2.imshow("Cropped", cropped_image)
 
         if type == 1:
-            cropped_image = image[cord[0][1][0][0]:cord[0][1][1][0], cord[0][1][0][1]:cord[0][1][1][1]]
-            binary_image = cv2.inRange(cropped_image, lower_red, higher_red)
-            ship_positions = extract_blobs(binary_image)
-            cv2.imshow("Cropped", binary_image)
+            cropped_image = image[cord[1][0][0][1]:cord[0][0][1][1], cord[1][0][0][0]:cord[0][1][1][0]]
+            #binary_image = cv2.inRange(cropped_image, lower_red, higher_red)
+            #ship_positions = extract_blobs(binary_image)
+            cv2.imshow("Cropped", cropped_image)
 
     elif player == 2:
         if type == 0:
-            cropped_image = image[cord[1][1][0][0]:cord[1][1][1][0], cord[1][1][0][1]:cord[1][1][1][1]]
+            cropped_image = image[cord[0][1][0][1]:cord[0][1][1][1], cord[0][1][0][0]:cord[0][1][1][0]]
             #binary_image = cv2.inRange(cropped_image, lower_red, higher_red)
-            ship_positions = extract_blobs(binary_image)
-            cv2.imshow("Cropped", binary_image)
+            #ship_positions = extract_blobs(binary_image)
+            cv2.imshow("Cropped", cropped_image)
 
         if type == 1:
-            cropped_image = image[cord[0][0][0][0]:cord[0][0][1][0], cord[0][0][0][1]:cord[0][0][1][1]]
+            cropped_image = image[cord[1][1][0][1]:cord[1][1][1][1], cord[1][1][0][0]:cord[1][1][1][0]]
             #binary_image = cv2.inRange(cropped_image, lower_red, higher_red)
-            ship_positions = extract_blobs(binary_image)
-            cv2.imshow("Cropped", binary_image)
+            #ship_positions = extract_blobs(binary_image)
+            cv2.imshow("Cropped", cropped_image)
 
 video = cv2.VideoCapture(1)
-#cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-#cap.set(cv2.CAP_PROP_EXPOSURE, -3)
+video.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+video.set(cv2.CAP_PROP_EXPOSURE, -3)
 
 while True:
     check, frame = video.read()
-    cv2.imshow("Frame", fram)
+    cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
 
 
 
-#print(positions[1][0][0][0], positions[1][0][1][0], positions[1][0][0][1], positions[1][0][1][1])
-#cropped_image = frame[positions[1][0][0][0]:positions[1][0][1][0], positions[1][0][0][1]:positions[1][0][1][1]]
-#cv2.imshow(",", cropped_image)
-detectShapes(frame, 2, 0, positions)
+detectShapes(frame, 2, 1, positions)
 video.release()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
