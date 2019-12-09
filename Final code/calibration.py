e6c7d94cbcde4dca120b39bdf57cfd22955727c9
dmocko18 <50201406+dmocko18@users.noreply.github.com>
2019-12-09 13:15:51 +0100

    Merge branch 'master' of C:\Users\David\PycharmProjects\P3_Battleships_Public with conflicts.

--- Final code/calibration.py
+++ Final code/calibration.py
@@ -1,8 +1,9 @@
-    # This modules purpose is to find the coordinates for each grid
+# This modules purpose is to find the coordinates for each grid
 import cv2
 import numpy as np
 from blob import extract_blobs
 
+# image = cv2.imread("/Users/mikkelsangmeebaunsgaard/Desktop/mikkelTHIS.jpg")
 
 def coordinates(blobs):
     coordinates = []
@@ -28,20 +29,21 @@ def calibration(image_to_crop):
     binary_image = cv2.inRange(blurred_image, 130, 255)
     cv2.imshow("gray", blurred_image)
     cv2.imshow("mask", binary_image)
-
     # Find blob
-
     image_blobs = extract_blobs(binary_image)
+    #image_blobs.pop()
+
     correct_blobs = []
 
     for blob in image_blobs:
-        if len(blob) > 17000:
-
+        if len(blob) > 50000:
             correct_blobs.append(blob)
 
     coord = coordinates(correct_blobs)
 
-    print(coord)
+    #for blob in image_blobs:
+    #    print(blob[0], blob[-1])
+
     # Find placement grids
     player1_placement = (coord[0][0], coord[0][-1])
     player2_placement = (coord[-1][0], coord[-1][-1])
@@ -52,14 +54,10 @@ def calibration(image_to_crop):
 
     # Return the coordinates to the function, hence turning the function into these values
     # These values can be accessed by refering to them as an array
-
     return ((player1_placement, player2_placement), (player1_attack, player2_attack))
 
-cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-cv2.imshow("window", cv2.imread("Pictures/fourblobs.png")):
 
-video = cv2.VideoCapture(1)
+video = cv2.VideoCapture(0)
 #video.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
 #video.set(cv2.CAP_PROP_EXPOSURE, -3)
 #video.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
@@ -70,13 +68,18 @@ video = cv2.VideoCapture(1)
 temp = 0
 while True:
     check, frame = video.read()
+    #cv2.imshow("frame", frame)
     if temp == 20:
         break
 
     temp += 1
 
+#cv2.imshow("frame", frame)
+#cv2.imwrite("/Users/mikkelsangmeebaunsgaard/Desktop/whiteblobs.jpg", frame)
+#cv2.imwrite("Pictures/withoutSquares.jpg", frame)
 positions = calibration(frame)
 
+print(positions)
 video.release()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
