0087b53b1b643435a0812a1efd041e7b281c1ef9
Mikkel Sang Mee Baunsgaard <baunsgaard38@gmail.com>
2019-12-09 19:53:58 +0100

    ..

--- Final code/main.py
+++ Final code/main.py
@@ -6,6 +6,7 @@ from cropAndDetect import *
 from detectShapePosition import *
 from PlacementModule import *
 from Ship import ship
+import random
 
 state = True
 player1_pressed = False; player2_pressed = False
@@ -15,21 +16,21 @@ rows, cols = (10, 10)
 pap1 = [[0 for i in range(cols)] for j in range(rows)]; feed1 = [[0 for ii in range(cols)] for jj in range(rows)]; p1ships = [];
 pap2 = [[0 for f in range(cols)] for s in range(rows)]; feed2 = [[0 for ff in range(cols)] for ss in range(rows)]; p2ships = [];
 
-for shipPos in detectShapePosition(background_images[0], background_images[0]):
+for shipPos in detectShapePosition(background_images[0], shapes[0], 14):
     PlaceShip(pap1, shipPos, p1ships)
 
-for minePos in detectShapePosition(background_images[-1], shapes[-1]):
+for minePos in detectShapePosition(background_images[-1], shapes[-1], 14):
     CheckMines(minePos, pap1, feed1)
 
-for shipPos in detectShapePosition(background_images[2], shapes[2]):
+for shipPos in detectShapePosition(background_images[1], shapes[1], 10):
     PlaceShip(pap2, shipPos, p2ships)
 
-for minePos in detectShapePosition(background_images[1], shapes[1]):
+for minePos in detectShapePosition(background_images[2], shapes[2], 10):
     CheckMines(minePos, pap2, feed2)
 
 
 
-"""
+
 print("PLAYER 1")
 for row in pap1:
     print(row)
@@ -39,7 +40,9 @@ for s in p1ships:
 
 for row in feed1:
     print(row)
-"""
+
+
+print("##############")
 
 
 print("PLAYER 2")
