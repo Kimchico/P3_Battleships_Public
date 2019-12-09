7bce78403cc84a71f9a5a209a120565e0105fb3a
Kimchico <45200859+Kimchico@users.noreply.github.com>
2019-12-09 13:29:24 +0100

    Making BG subtraction in TurnHandeling, not done

--- TurnHandeling.py
+++ TurnHandeling.py
@@ -1,51 +1,73 @@
 import cv2
 import numpy as np
+import time
 #Video capture, the number is which camera, 0 is laptop, 1 is webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 #maybe make a loop here, where it will capture first 20 frames
+delayCounter = 0
 frameCount = 0
 images = []
+startTime = time.time()
+#ret, firstFrame = cap.read()
 
-ret, firstFrame = cap.read()
+#def denoise(firstFrame):
 
-def denoise(firstFrame):
+    #firstFrame = cv2.medianBlur(firstFrame, 5)
+    #firstFrame = cv2.GaussianBlur(firstFrame, (7, 7), 0)
 
-    firstFrame = cv2.medianBlur(firstFrame, 5)
-    firstFrame = cv2.GaussianBlur(firstFrame, (7, 7), 0)
+    #return firstFrame
+#firstFrame = denoise(firstFrame)
+#gray = cv2.cvtColor(firstFrame, cv2.COLOR_BGR2GRAY)
 
-    return firstFrame
-firstFrame = denoise(firstFrame)
-gray = cv2.cvtColor(firstFrame, cv2.COLOR_BGR2GRAY)
+#while True:
 
-while True:
+    #images.append(gray)
+    #if frameCount == 19:
+     #   break
+    #frameCount += 1
 
-    images.append(gray)
-    if frameCount == 19:
-        break
-    frameCount += 1
+#print(len(images))
 
-print(len(images))
-
-meanImage = sum(images) / len(images)
-print(meanImage)
+#meanImage = sum(images) / len(images)
+#print(meanImage)
 
 
 #Run through the frame continuously
 while True:
 
+
         ret, frame = cap.read()
+        ret, firstFrame = cap.read()
+        gray = cv2.cvtColor(firstFrame, cv2.COLOR_BGR2GRAY)
 
 
+        if time.time() - startTime >= 50:
+            ret, firstFrame = cap.read()
+            secFrame = "openCV10SecFrame.png".format(delayCounter)
+            cv2.imwrite(secFrame, firstFrame)
+            startTime = time.time()
+        delayCounter += 1
+
+
+        def denoise(firstFrame):
+
+            firstFrame = cv2.medianBlur(firstFrame, 5)
+            firstFrame = cv2.GaussianBlur(firstFrame, (9, 9), 0)
+
+            return firstFrame
+
+
+        firstFrame = denoise(firstFrame)
         frame = denoise(frame)
 
         gray2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         #substraction = gray2 - gray
         #substraction = grey - [images for image in images]
         #substraction = gray2 - [gray for image in gray]
-        #1subtraction = cv2.absdiff(gray2, gray)
-        subtraction = cv2.absdiff(gray2, [images for image in images])
-        ret, binary = cv2.threshold(subtraction, 15, 255, cv2.THRESH_BINARY)
+        subtraction = cv2.absdiff(gray2, gray)
+        #subtraction = cv2.absdiff(gray2, [images for image in images])
+        ret, binary = cv2.threshold(subtraction, 5, 255, cv2.THRESH_BINARY)
 
         cv2.imshow('soething', firstFrame)
         cv2.imshow('first frame', gray)
