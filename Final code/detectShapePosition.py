ac71cb92dd8494ff978acffd0235d8c15badc504
Acalin10 <calinmares10@gmail.com>
2019-12-09 15:54:36 +0100

    -detection now working

--- Final code/detectShapePosition.py
+++ Final code/detectShapePosition.py
@@ -14,15 +14,15 @@ def detectShapePosition(back,curent):
     current_greyBH = cv2.equalizeHist(current_grey)
     back_greyBH = cv2.GaussianBlur(back_greyBH, (7,7), 0)
     current_greyBH = cv2.GaussianBlur(current_greyBH, (7, 7), 0)
-    cv2.imshow('back',back_greyBH)
-    cv2.imshow('current',current_greyBH)
+    #cv2.imshow('back',back_greyBH)
+    #cv2.imshow('current',current_greyBH)
     difference = cv2.absdiff(back_greyBH, current_greyBH)
-    cv2.imshow('diff',difference)
+    #cv2.imshow('diff',difference)
     kernel = np.ones((5,5), np.uint8)
     img_binary = cv2.threshold(difference, 14,255, cv2.THRESH_BINARY)[1]
-    cv2.imshow('smth',img_binary)
+    #cv2.imshow('smth',img_binary)
     erosion = cv2.erode(img_binary,kernel,iterations=2)
-    cv2.imshow('dif',erosion)
+    #cv2.imshow('dif',erosion)
     cv2.imwrite( "Pictures/erosion.jpg", erosion)
     for point in find_shapes(erosion):
 
@@ -48,17 +48,21 @@ def shape_positions(coord,width,height):
 
     cubeWidth = int(width / 10)
     cubeHeight = int(height / 10)
+    '''
     print(str(width))
     print(height)
     print('Cube Width: ' + str(cubeWidth) + ' Cube Height: '+ str(cubeHeight))
+    '''
     shapeXMin = coord[0][0]
     shapeYMin = coord[0][1]
     shapeXMax = coord[1][0]
     shapeYMax = coord[1][1]
+    '''
     print(shapeXMin)
     print(shapeXMax)
     print(shapeYMin)
     print(shapeYMax)
+    '''
     shapeRowMin = 0
     shapeColumnMin = 0
     shapeRowMax = 0
@@ -68,34 +72,36 @@ def shape_positions(coord,width,height):
     yMin = 0
     yMax = 10
     while (xMin * cubeWidth) <= (shapeXMin) and xMin <= 10:
+        '''
         print('XMIN')
         print(str(xMin * cubeWidth) + ' is smaller than ' + str(shapeXMin))
+        '''
         shapeColumnMin = xMin
-        print(str(shapeColumnMin))
+        #print(str(shapeColumnMin))
         xMin += 1
     xMin=0
     #print('STOP')
     while (yMin * cubeHeight) <= (shapeYMin) and yMin <= 10:
-        print('YMIN')
-        print(str(yMin * cubeHeight) + ' is smaller than ' + str(shapeYMin))
+        #print('YMIN')
+        #print(str(yMin * cubeHeight) + ' is smaller than ' + str(shapeYMin))
         shapeRowMin = yMin
-        print(str(shapeRowMin))
+        #print(str(shapeRowMin))
         yMin += 1
     yMin=0
     #print('STOP')
     while (xMax * cubeWidth) >= shapeXMax  and xMax <= 10:
-        print('XMAX')
-        print(str(xMax * cubeWidth) + ' is bigger than ' + str(shapeXMax))
+        #print('XMAX')
+        #print(str(xMax * cubeWidth) + ' is bigger than ' + str(shapeXMax))
         shapeColumnMax = xMax
-        print(str(shapeColumnMax))
+        #print(str(shapeColumnMax))
         xMax -= 1
     xMax=0
     #print('STOP')
     while ((yMax * cubeHeight)) >= shapeYMax  and yMax <= 10:
-        print('YMAX')
-        print(str(yMax * cubeHeight) + ' is bigger than ' + str(shapeYMax))
+        #print('YMAX')
+        #print(str(yMax * cubeHeight) + ' is bigger than ' + str(shapeYMax))
         shapeRowMax = yMax
-        print(str(shapeRowMax))
+        #print(str(shapeRowMax))
         yMax -= 1
 
     yMax=0
@@ -115,7 +121,7 @@ def shape_positions(coord,width,height):
         if(diffRows>diffColumn):
             shapeColumnMax=shapeColumnMax-1
     yield(shapeColumnMin,shapeColumnMax,shapeRowMin,shapeRowMax)
-    print(str(shapeColumnMin) + ' ' + str(shapeColumnMax)+ ' '+str(shapeRowMin)+ ' '+str(shapeRowMax))
+    #print(str(shapeColumnMin) + ' ' + str(shapeColumnMax)+ ' '+str(shapeRowMin)+ ' '+str(shapeRowMax))
 def shape_gridP(MinMax):
     Vmin = MinMax[0]
     Vmax = MinMax[1]
@@ -142,14 +148,14 @@ def find_shapes(image):
     x=w/10
 
     for blob in blobs:
-        print('before removing '+ str(len(blob)))
+        #print('before removing '+ str(len(blob)))
         min_value = blob[0]
         max_value = blob[-1]
 
         if max_value[1]-min_value[1] < x*5 and max_value[0]-min_value[0] < y*5 and len(blob)>((x-2)*(y-2)):
             blobs2.append(blob)
-    for blob2 in blobs2:
-        print('after removing '+str(len(blob2)))
+    #for blob2 in blobs2:
+        #print('after removing '+str(len(blob2)))
     blobs3=coordinates(blobs2)
     for blob3 in blobs3:
         min_value = blob3[0]
