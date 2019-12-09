1400ec0ee306bdc62a007dc5287bec13e63aa64f
Kimchico <45200859+Kimchico@users.noreply.github.com>
2019-12-09 11:27:34 +0100

    Making BG subtraction in TurnHandeling, not done

--- TurnHandeling.py
+++ TurnHandeling.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 #Video capture, the number is which camera, 0 is laptop, 1 is webcam
-cap = cv2.VideoCapture (0)
+cap = cv2.VideoCapture(0)
 
 #maybe make a loop here, where it will capture first 20 frames
 frameCount = 0
@@ -9,37 +9,48 @@ images = []
 
 ret, firstFrame = cap.read()
 
+def denoise(firstFrame):
 
-#while True:
+    firstFrame = cv2.medianBlur(firstFrame, 5)
+    firstFrame = cv2.GaussianBlur(firstFrame, (7, 7), 0)
 
-#    images.append(firstFrame)
- #   if frameCount == 19:
-  #      break
-   # frameCount += 1
+    return firstFrame
+firstFrame = denoise(firstFrame)
+gray = cv2.cvtColor(firstFrame, cv2.COLOR_BGR2GRAY)
 
-#print(len(images))
+while True:
 
-#meanImage = sum(images) / len(images)
-#print(meanImage)
+    images.append(gray)
+    if frameCount == 19:
+        break
+    frameCount += 1
 
-#def denoise(frame):
+print(len(images))
 
- #   frame = cv2.medianBlur(frame, 5)
-  #  frame = cv2.GaussianBlur(frame, (7, 7), 0)
+meanImage = sum(images) / len(images)
+print(meanImage)
 
-   # return frame
 
 #Run through the frame continuously
 while True:
 
         ret, frame = cap.read()
-        substraction = frame - firstFrame
-        #substraction = frame - [images for image in images]
-        (thresh, bi) = cv2.threshold(substraction, 127, 255, cv2.THRESH_BINARY)
 
-        cv2.imshow('first frame', firstFrame)
-        cv2.imshow('webcam', frame)
-        cv2.imshow('difference',substraction)
+
+        frame = denoise(frame)
+
+        gray2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #substraction = gray2 - gray
+        #substraction = grey - [images for image in images]
+        #substraction = gray2 - [gray for image in gray]
+        #1subtraction = cv2.absdiff(gray2, gray)
+        subtraction = cv2.absdiff(gray2, [images for image in images])
+        ret, binary = cv2.threshold(subtraction, 15, 255, cv2.THRESH_BINARY)
+
+        cv2.imshow('soething', firstFrame)
+        cv2.imshow('first frame', gray)
+        cv2.imshow('webcam', gray2)
+        cv2.imshow('difference', binary)
 
         key = cv2.waitKey(20)
 
