06a926df487c795b1dc1707e385df325f0d489f3
Acalin10 <calinmares10@gmail.com>
2019-12-10 17:05:04 +0100

    -detection now working

--- Final code/attack.py
+++ Final code/attack.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
 def attack(coord,feedArray,placementArray,shipsArray):
-    a=coord[0][1]
-    b=coord[0][0]
+    x=coord[0][1]
+    y=coord[0][0]
     #for p in placementArray: #this might need to be modified
-    if placementArray[a][b]==1:
-        feedArray[a][b]=2 #2 is shot and hit
-    if placementArray[a][b]==0:
-        feedArray[a][b]=3 #3 is shot and missed
+    if placementArray[x][y]==1:
+        feedArray[x][y]=2 #2 is shot and hit
+    if placementArray[x][y]==0:
+        feedArray[x][y]=3 #3 is shot and missed
     for i in shipsArray:
         if i.isDestroyed == False:
             for j in i.positions:
-                if j[0]==a and j[1]==b:
+                if j[0]==x and j[1]==y:
                     i.health=i.health-1
