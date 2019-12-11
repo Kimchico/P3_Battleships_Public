dc5a72d96c122b256bb9307d8fa140c3663e4d6c
Mikkel Sang Mee Baunsgaard <baunsgaard38@gmail.com>
2019-12-11 16:49:14 +0100

    ,

--- fullscreen_.py
+++ fullscreen_.py
@@ -1,57 +1,20 @@
 # import pygame module in this program
 import pygame
+from pygame.locals import *
 
-# activate the pygame library .
-# initiate pygame and give permission
-# to use pygame's functionality.
 pygame.init()
 
-# define the rgb value
-# for white colour
-white = (255, 255, 255)
+image = pygame.image.load("FOURBLOBS.png")
 
-# assigning values to x and y variable
-x = 400
-y = 400
-
-# create the display surface object
-# of specific dimension..e(x, y).
-display_surface = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
-
-# set the pygame window name
+window = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
 pygame.display.set_caption('image')
 
-# create a surface object, image is drawn on it.
-image = pygame.image.load("FOURBLOBS.png")
-image = pygame.transform.scale(image, (1920, 1080))
 
-
-# infinite loop
 while True:
-
-    # completely fill the surface object
-    # with white colour
-    display_surface.fill(white)
-
-    # copying the image surface object
-    # to the display surface object at
-    # (0, 0) coordinate.
-    display_surface.blit(image, (0, 0))
-
-    # iterate over the list of event objects
-    # that was returned by pygame.event.get() method.
+    window.blit(image, (0, 0))
     for event in pygame.event.get():
-
-        # if event object type is quit
-        # then quitting the pygame
-        # and program both.
-        if event.type == pygame.quit:
-
-            # deactivates the pygame library
+        if event.type == pygame.KEYDOWN:
             pygame.quit()
-
-            # quit the program.
             quit()
 
-        # draws the surface object to the screen.
         pygame.display.update()
