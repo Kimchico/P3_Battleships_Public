e0385102f1f55d1bc0cb0b1486f25fa55b66bd8d
Acalin10 <calinmares10@gmail.com>
2019-12-14 16:36:38 +0100

    -fixed barrage and torpedo(maybe)

--- Final code/attack.py
+++ Final code/attack.py
@@ -4,23 +4,28 @@ import os
 
 def attack(coord,feedArray,placementArray,shipsArray):
     print(coord)
-    x=coord[0][0]
-    y=coord[0][1]
-    x = 9 - x
-    y = 9 - y
+    for c in coord:
+        x = c[0]
+        y = c[1]
+        if y == 10:
+            y = y - 1
+        if x == 10:
+            x = x - 1
+        x = 9 - x
+        y = 9 - y
 
-    for i in shipsArray:
-        if i.isDestroyed == False:
-            for j in i.positions:
-                if j[0]==x and j[1]==y:
-                    i.health=i.health-1
+        for i in shipsArray:
+            if i.isDestroyed == False:
+                for j in i.positions:
+                    if j[0]==x and j[1]==y:
+                        i.health=i.health-1
 
-    #for p in placementArray: #this might need to be modified
-    if placementArray[y][x]==1:
-        feedArray[y][x]=2 #2 is shot and hit
-        os.system("say 'You hit'")
-        return True
-    if placementArray[y][x]==0:
-        feedArray[y][x]=3 #3 is shot and missed
-        os.system("say 'You miss'")
-        return False
+        #for p in placementArray: #this might need to be modified
+        if placementArray[y][x]==1:
+            feedArray[y][x]=2 #2 is shot and hit
+            os.system("say 'You hit'")
+            return True
+        if placementArray[y][x]==0:
+            feedArray[y][x]=3 #3 is shot and missed
+            os.system("say 'You miss'")
+            return False
